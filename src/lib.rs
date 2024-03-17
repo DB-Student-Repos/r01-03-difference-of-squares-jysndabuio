760d3b4bcf5ef3b29146657d57b65a97e932bd07
jysndabuio <jysndabu@outlook.com>
2024-03-17 15:17:21 +0100

    My soulution

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,31 @@
 pub fn square_of_sum(n: u32) -> u32 {
-    unimplemented!("square of sum of 1...{n}")
+    let mut square_of_sum:u32 = 0;
+
+    for x in 1..=n {
+        square_of_sum += x;
+    }
+    return square_of_sum*square_of_sum;
 }
 
 pub fn sum_of_squares(n: u32) -> u32 {
-    unimplemented!("sum of squares of 1...{n}")
+    let mut sum_of_squares:u32 = 0;
+
+    for x in 1..=n {
+        sum_of_squares += x*x;
+    }
+    return sum_of_squares
 }
 
 pub fn difference(n: u32) -> u32 {
-    unimplemented!("difference between square of sum of 1...{n} and sum of squares of 1...{n}")
+    let mut square_of_sum:u32 = 0;
+    let mut sum_of_squares:u32 = 0;
+
+    for x in 1..=n {
+        square_of_sum += x;
+    }
+    for x in 1..=n {
+        sum_of_squares += x*x;
+    }
+    return square_of_sum - sum_of_squares;
 }
+
