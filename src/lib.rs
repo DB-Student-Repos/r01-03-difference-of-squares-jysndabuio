0ad35404c10c8ad264ac39475695a610dadb11d4
jysndabuio <jysndabu@outlook.com>
2024-03-20 09:16:07 +0100

    Revision

--- src/lib.rs
+++ src/lib.rs
@@ -1,19 +1,9 @@
 pub fn square_of_sum(n: u32) -> u32 {
-    let mut square_of_sum:u32 = 0;
-
-    for x in 1..=n {
-        square_of_sum += x;
-    }
-    return square_of_sum*square_of_sum;
+    (1..=n).sum::<u32>().pow(2) // Better use methods .sum() & .pow, rather than using for loop.
 }
 
 pub fn sum_of_squares(n: u32) -> u32 {
-    let mut sum_of_squares:u32 = 0;
-
-    for x in 1..=n {
-        sum_of_squares += x*x;
-    }
-    return sum_of_squares
+    (1..=n).map(|x| x * x).sum::<u32>() // .map method call in the range to make transformation which is |x| x * x, then add the transformed range.
 }
 
 pub fn difference(n: u32) -> u32 {
